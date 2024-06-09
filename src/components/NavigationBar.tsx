ad0d7ee8ef025f3c859aa082d1790f739e1ab1eb
Rebeca\Rebe y Joa <joaquin_trujillo@icloud.com>
2024-06-09 15:50:48 +0200

    fix: update Link instead of a

--- src/components/NavigationBar.tsx
+++ src/components/NavigationBar.tsx
@@ -4,10 +4,10 @@ import Link from 'next/link'
 export default function NavigationBar() {
   return (
     <header className='px-4 lg:px-6 h-14 flex items-center sticky top-0 backdrop-blur-xl'>
-      <a className='flex items-center justify-center' href='/'>
+      <Link className='flex items-center justify-center gap-1' href='/'>
         <NoteIcon />
-        <span className='sr-only'>Music Sharing Platform</span>
-      </a>
+        <span>Trackshare</span>
+      </Link>
       <nav className='ml-auto flex gap-4 sm:gap-6'>
         <Link
           className='text-sm font-medium hover:underline underline-offset-4'
