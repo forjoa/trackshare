5d9ec247ff94f5b13cc37eab0d21854e32c3041e
Rebeca\Rebe y Joa <joaquin_trujillo@icloud.com>
2024-06-12 22:48:37 +0200

    build: create Input ui component

---
+++ src/components/ui/Input.tsx
@@ -0,0 +1,8 @@
+export default function Input(props: any) {
+  return (
+    <input
+      className='flex h-10 w-full rounded-md border px-3 py-2 text-sm focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50'
+      {...props}
+    />
+  )
+}
