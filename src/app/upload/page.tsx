95b350abc585f3d7e54241ffef2357de445a257c
Rebeca\Rebe y Joa <joaquin_trujillo@icloud.com>
2024-06-14 15:48:29 +0200

    get user information correctly

--- src/app/upload/page.tsx
+++ src/app/upload/page.tsx
@@ -1,15 +1,24 @@
+'use client'
+import { useEffect, useState } from 'react'
 import LoginRegister from '@/components/LoginRegister'
+import jwt from 'jsonwebtoken'
+import { Artist } from '@/lib/types'
 
 export default function Upload() {
-  let user = null
+  const [user, setUser] = useState<Artist | null>(null)
 
-  if (typeof window !== 'undefined') {
-    user = localStorage.getItem('user')
-  }
+  useEffect(() => {
+    if (typeof window !== 'undefined') {
+      const storedUser = localStorage.getItem('user')
+      setUser(jwt.decode(storedUser as string) as unknown as Artist)
+    }
+  }, [])
+
+  console.log(user)
 
   if (!user) {
     return <LoginRegister />
   }
 
-  return <h1>upload</h1>
+  return <h1>Upload</h1>
 }
