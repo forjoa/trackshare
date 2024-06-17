a47eaa56623999afe963ee490a8e2c73ffc2c652
Rebeca\Rebe y Joa <joaquin_trujillo@icloud.com>
2024-06-17 19:41:05 +0200

    build(endpoint): upload function to upload a song

--- src/app/api/uploadSong/route.ts
+++ src/app/api/uploadSong/route.ts
@@ -6,11 +6,56 @@ export async function POST(req: NextRequest) {
   try {
     const body = await req.json()
 
+    const { artistId, song, platforms } = body
+
+    if (!artistId || !song || !platforms || !Array.isArray(platforms)) {
+      return NextResponse.json(
+        {
+          success: false,
+          message: 'Invalid input data',
+        },
+        {
+          status: 400,
+        }
+      )
+    }
+
+    const result = await db.execute({
+      sql: 'INSERT INTO songs(title, artist_id) VALUES (?, ?)',
+      args: [song, artistId],
+    })
+
+    const songId = result.lastInsertRowid
+
+    for (let i = 0; i < platforms.length; i++) {
+      const { platform, link } = platforms[i]
+      if (!platform || !link) {
+        return NextResponse.json(
+          {
+            success: false,
+            message: `Invalid platform data at index ${i}`,
+          },
+          {
+            status: 400,
+          }
+        )
+      }
+
+      await db.execute({
+        sql: 'INSERT INTO platforms(platform, link, song_id) VALUES (?, ?, ?)',
+        args: [platform, link, songId],
+      })
+    }
+
+    return NextResponse.json({
+      success: true,
+      message: 'Song and platforms inserted correctly!',
+    })
   } catch (error) {
     return NextResponse.json(
       {
         success: false,
-        message: 'Error while sending the data',
+        message: `Error while sending the data`,
       },
       {
         status: 500,
