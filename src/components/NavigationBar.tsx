9b656355484588e14bcc0d895fae32973ed2e261
Rebeca\Rebe y Joa <joaquin_trujillo@icloud.com>
2024-06-18 17:51:16 +0200

    build(link): new link to access to my songs

--- src/components/NavigationBar.tsx
+++ src/components/NavigationBar.tsx
@@ -1,7 +1,20 @@
+'use client'
 import { NoteIcon } from '@/assets/icons'
+import { Artist } from '@/lib/types'
 import Link from 'next/link'
+import jwt from 'jsonwebtoken'
+import { useEffect, useState } from 'react'
 
 export default function NavigationBar() {
+  const [user, setUser] = useState<Artist | null>(null)
+
+  useEffect(() => {
+    if (typeof window !== 'undefined') {
+      const storedUser = localStorage.getItem('user')
+      setUser(jwt.decode(storedUser as string) as unknown as Artist)
+    }
+  }, [])
+
   return (
     <header className='px-4 lg:px-6 h-14 flex items-center sticky top-0 backdrop-blur-xl z-50'>
       <Link className='flex items-center justify-center gap-1' href='/'>
@@ -15,6 +28,14 @@ export default function NavigationBar() {
         >
           Explore
         </Link>
+        {user && (
+          <Link
+            className='text-sm font-medium hover:underline underline-offset-4'
+            href='/mysongs'
+          >
+            My songs
+          </Link>
+        )}
         <Link
           className='text-sm font-medium hover:underline underline-offset-4 bg-white text-black py-2 px-4 rounded'
           href='/upload'
