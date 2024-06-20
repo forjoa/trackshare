5b360e58ef8de94407eee70d723e49e1cb0bba97
Rebeca\Rebe y Joa <joaquin_trujillo@icloud.com>
2024-06-20 09:05:14 +0200

    build(icon): upload Upload icon

--- src/assets/icons.tsx
+++ src/assets/icons.tsx
@@ -116,3 +116,23 @@ export const Youtube = () => (
     <path fill='#FFF' d='m102.421 128.06 66.328-38.418-66.328-38.418z' />
   </svg>
 )
+
+export const UploadIcon = () => (
+  <svg
+    xmlns='http://www.w3.org/2000/svg'
+    width='24'
+    height='24'
+    viewBox='0 0 24 24'
+    fill='none'
+    stroke='#ffffff'
+    strokeWidth='2'
+    strokeLinecap='round'
+    strokeLinejoin='round'
+    className='icon icon-tabler icons-tabler-outline icon-tabler-upload'
+  >
+    <path stroke='none' d='M0 0h24v24H0z' fill='none' />
+    <path d='M4 17v2a2 2 0 0 0 2 2h12a2 2 0 0 0 2 -2v-2' />
+    <path d='M7 9l5 -5l5 5' />
+    <path d='M12 4l0 12' />
+  </svg>
+)
