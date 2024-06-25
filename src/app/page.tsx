13ab16e174d1a0bd5e07022f44d615220fd42330
Rebeca\Rebe y Joa <joaquin_trujillo@icloud.com>
2024-06-25 17:23:50 +0200

    refactor: update key frames subtitle colors

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -78,7 +78,7 @@ export default function Home() {
         <div className='container space-y-12 px-4 md:px-6'>
           <div className='flex flex-col items-center justify-center space-y-4 text-center'>
             <div className='space-y-2'>
-              <div className='inline-block rounded-lg bg-gray-100 px-3 py-1 text-sm dark:bg-gray-800'>
+              <div className='inline-block rounded-lg px-3 py-1 text-sm bg-[#6C3D21] border border-[#FF822E]'>
                 Key Features
               </div>
               <h2 className='text-3xl font-bold tracking-tighter sm:text-5xl'>
