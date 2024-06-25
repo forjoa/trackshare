d2470d28165c0073302d8d4fe73ed0120ee257af
Rebeca\Rebe y Joa <joaquin_trujillo@icloud.com>
2024-06-25 17:39:15 +0200

    refactor: border colors

--- src/components/KeyFeature.tsx
+++ src/components/KeyFeature.tsx
@@ -5,7 +5,7 @@ interface KeyFeatureProps {
 
 export default function KeyFeature({ title, content }: KeyFeatureProps) {
   return (
-    <div className='grid gap-1 border border-transparent p-4 hover:border-gray-800 rounded transition-all'>
+    <div className='grid gap-1 border border-transparent p-4 hover:border-[#6C3D21] rounded transition-all'>
       <h3 className='text-lg font-bold'>{title}</h3>
       <p className='text-sm text-gray-500 dark:text-gray-400'>{content}</p>
     </div>
